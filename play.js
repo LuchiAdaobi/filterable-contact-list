124c870f36ec96e809ab4c3865aaba17cba19765
Luchi <luchichronicles@gmail.com>
2023-11-09 12:51:38 -0800

    used a map method on the nodelist instead of for loop in the play.js file

--- play.js
+++ play.js
@@ -1,24 +1,48 @@
 const input = document.getElementById("filterInput");
 const lis = document.querySelectorAll("ul li.collection-item");
-const noContact = document.getElementById('no-contact')
+const noContact = document.getElementById("no-contact");
 
 // input.addEventListener("keyup", filterNames);
 
-input.addEventListener('keyup', ()=> {
-      let inputValue = input.value.toUpperCase();
+input.addEventListener("keyup", () => {
+  //   let inputValue = input.value.toUpperCase();
+  //   let contactsFound = false;
 
-      for (let i = 0; i < lis.length; i++) {
-        let index = lis[i];
-        let a = index.getElementsByTagName("a")[0];
-        let text = a.innerHTML.toUpperCase();
+  //   for (let i = 0; i < lis.length; i++) {
+  //     let index = lis[i];
+  //     let a = index.getElementsByTagName("a")[0];
+  //     let text = a.innerHTML.toUpperCase();
 
-        if (text.indexOf(inputValue) > -1) {
-          index.style.display = "";
-        } else {
-          index.style.display = "none";
-        }
-      }
-})
+  //     if (text.indexOf(inputValue) > -1) {
+  //       index.style.display = "";
+  //       contactsFound = true
+  //     } else {
+  //       index.style.display = "none";
+  //     }
+  // }
+
+  let inputValue = input.value.toUpperCase();
+  let contactsFound = false;
+
+  Array.from(lis).map((li) => {
+    let a = li.getElementsByTagName("a")[0];
+    let text = a.innerHTML.toUpperCase();
+
+    if (text.includes(inputValue)) {
+      li.style.display = "";
+      contactsFound = true;
+    } else {
+      li.style.display = "none";
+    }
+
+    if (contactsFound) {
+      noContact.style.display = "none";
+    } else {
+      noContact.style.display = "block";
+    }
+  });
+  
+});
 
 function filterNames() {
   let inputValue = input.value.toUpperCase();
@@ -34,7 +58,7 @@ function filterNames() {
       index.style.display = "none";
     }
   }
-};
+}
 
 function filterNames1() {
   // get value of input
